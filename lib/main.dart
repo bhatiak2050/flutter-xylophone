068b8c2bc4b8a8128fca3ae9757b64ff587214c8
bhatiak2050 <karanbhatia2050@gmail.com>
2020-04-29 17:58:31 +0530

    added flatbuttons w functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  FlatButton drawButton(int no, MaterialColor color){
+    return FlatButton(
+      color: color,
+      child: Text(no.toString()),
+      onPressed: () {
+        final player = AudioCache();
+        player.play('note$no.wav');
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                drawButton(1, Colors.red),
+                drawButton(2, Colors.orange),
+                drawButton(3, Colors.yellow),
+                drawButton(4, Colors.green),
+                drawButton(5, Colors.teal),
+                drawButton(6, Colors.blue),
+                drawButton(7, Colors.purple)
+              ],
+            ),
+          ),
         ),
       ),
     );
