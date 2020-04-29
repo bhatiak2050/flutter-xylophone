6b13a5bdb6643a57c16cb53f4f7aed22215a0d6f
bhatiak2050 <karanbhatia2050@gmail.com>
2020-04-29 18:37:28 +0530

    use arrow functions

--- lib/main.dart
+++ lib/main.dart
@@ -4,41 +4,39 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  Expanded drawButton({int no, MaterialColor color}) {
-    return Expanded(
-      child: FlatButton(
-        color: color,
-        onPressed: () {
-          final player = AudioCache();
-          player.play('note$no.wav');
-        },
-        child: null,
-      ),
-    );
-  }
+  Expanded drawButton({int no, MaterialColor color}) => 
+      Expanded(
+        child: FlatButton(
+          color: color,
+          onPressed: () {
+            final player = AudioCache();
+            player.play('note$no.wav');
+          },
+          child: null,
+        ),
+      );
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                drawButton(no: 1, color: Colors.red),
-                drawButton(no: 2, color: Colors.orange),
-                drawButton(no: 3, color: Colors.yellow),
-                drawButton(no: 4, color: Colors.green),
-                drawButton(no: 5, color: Colors.teal),
-                drawButton(no: 6, color: Colors.blue),
-                drawButton(no: 7, color: Colors.purple)
-              ],
+  Widget build(BuildContext context) => 
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.black,
+          body: SafeArea(
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  drawButton(no: 1, color: Colors.red),
+                  drawButton(no: 2, color: Colors.orange),
+                  drawButton(no: 3, color: Colors.yellow),
+                  drawButton(no: 4, color: Colors.green),
+                  drawButton(no: 5, color: Colors.teal),
+                  drawButton(no: 6, color: Colors.blue),
+                  drawButton(no: 7, color: Colors.purple)
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    );
-  }
+      );
 }
